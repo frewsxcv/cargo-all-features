c920ba7e32657035962a85f254fba34e23eeb6f3
Corey Farwell <coreyf@rwell.org>
2020-05-25 11:09:13 -0400

    fix errors after crate rename

--- src/bin/cargo-build-all-features.rs
+++ src/bin/cargo-build-all-features.rs
@@ -1,15 +1,15 @@
 use std::{error, process};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let packages = cargo_test_all_features::determine_packages_to_test()?;
+    let packages = cargo_all_features::determine_packages_to_test()?;
 
     for package in packages {
-        let outcome = cargo_test_all_features::test_all_features_for_package(
+        let outcome = cargo_all_features::test_all_features_for_package(
             &package,
-            cargo_test_all_features::test_runner::CargoCommand::Build,
+            cargo_all_features::test_runner::CargoCommand::Build,
         )?;
 
-        if let cargo_test_all_features::TestOutcome::Fail(exit_status) = outcome {
+        if let cargo_all_features::TestOutcome::Fail(exit_status) = outcome {
             process::exit(exit_status.code().unwrap());
         }
     }
