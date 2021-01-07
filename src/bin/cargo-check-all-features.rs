3f582403c4556336e6175ef5eae550709f5f1698
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-01-07 11:53:39 +0800

    Add 'check-all-features' sub-command

---
+++ src/bin/cargo-check-all-features.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::error;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    if let Some(arg) = env::args().skip(1).next() {
+        if arg == "--help" {
+            println!("See https://crates.io/crates/cargo-all-features");
+            return Ok(());
+        }
+    }
+    cargo_all_features::run(cargo_all_features::test_runner::CargoCommand::Build)
+}
