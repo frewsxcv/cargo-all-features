432391929940487bbdf5fc61f5a30f0d6f13a7f8
person93 <person93.person93@gmail.com>
2021-11-29 13:01:16 -0500

    fix: handle subcommand and recognizing bin name properly

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use cargo_all_features::{run, test_runner::CargoCommand};
-use clap::{crate_authors, crate_description, crate_license, crate_version, Parser};
+use clap::{crate_authors, crate_description, crate_license, crate_name, crate_version, Parser};
 use std::error::Error;
 
 #[derive(Parser)]
 #[clap(
-    name = "cargo all-features",
+    name = crate_name!(),
     author = crate_authors!(),
     version = crate_version!(),
     about = crate_description!(),
     license = crate_license!(),
+    bin_name = "cargo all-features",
+    visible_alias = "all-features",
 )]
 struct Opts {
     /// choose which cargo command to run on the feature matrix: build, check, test
