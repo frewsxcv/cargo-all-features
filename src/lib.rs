4896cb4da46b0e86460a486e5fc2589c9ddbab13
Robert Collins <robertc@robertcollins.net>
2023-05-09 23:17:48 +0200

    Fix #39: permit building only root package
    At the root dir of Cargo workspaces are either a virtual manifest, or a
    root package. For root packages, we cannot rely on the heuristic of
    being in a particular sub-dir to locate a single package to run. The new
    option `--root-only` selects the single-package logic in this case.

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,24 @@ pub mod test_runner;
 mod types;
 
 pub fn run(cargo_command: test_runner::CargoCommand) -> Result<(), Box<dyn error::Error>> {
-    if let Some(arg) = env::args().nth(1) {
-        if arg == "--help" {
-            println!("See https://crates.io/crates/cargo-all-features");
-            return Ok(());
+    let (root_only, args) = if let Some(arg) = env::args().nth(2) {
+        match &arg[..] {
+            "--help" => {
+                println!("See https://crates.io/crates/cargo-all-features");
+                return Ok(());
+            }
+            "--root-only" => (true, env::args().skip(3)),
+            _ => (false, env::args().skip(2)),
         }
-    }
+    } else {
+        (false, env::args().skip(2))
+    };
+    let args = args.collect::<Vec<_>>();
 
-    let packages = determine_packages_to_test()?;
+    let packages = determine_packages_to_test(root_only)?;
 
     for package in packages {
-        let outcome = test_all_features_for_package(&package, cargo_command)?;
+        let outcome = test_all_features_for_package(&package, cargo_command, &args)?;
 
         if let TestOutcome::Fail(exit_status) = outcome {
             process::exit(exit_status.code().unwrap());
@@ -29,6 +36,7 @@ pub fn run(cargo_command: test_runner::CargoCommand) -> Result<(), Box<dyn error
 fn test_all_features_for_package(
     package: &cargo_metadata::Package,
     command: crate::test_runner::CargoCommand,
+    args: &[String],
 ) -> Result<TestOutcome, Box<dyn error::Error>> {
     let feature_sets = crate::features_finder::fetch_feature_sets(package);
 
@@ -42,6 +50,7 @@ fn test_all_features_for_package(
                 .parent()
                 .expect("could not find parent of cargo manifest path")
                 .to_owned(),
+            args,
         );
 
         let outcome = test_runner.run()?;
@@ -56,11 +65,13 @@ fn test_all_features_for_package(
     Ok(TestOutcome::Pass)
 }
 
-fn determine_packages_to_test() -> Result<Vec<cargo_metadata::Package>, Box<dyn error::Error>> {
+fn determine_packages_to_test(
+    root_only: bool,
+) -> Result<Vec<cargo_metadata::Package>, Box<dyn error::Error>> {
     let current_dir = env::current_dir()?;
     let metadata = cargo_metadata::fetch()?;
 
-    Ok(if current_dir == metadata.workspace_root {
+    Ok(if !root_only && current_dir == metadata.workspace_root {
         metadata
             .packages
             .iter()
